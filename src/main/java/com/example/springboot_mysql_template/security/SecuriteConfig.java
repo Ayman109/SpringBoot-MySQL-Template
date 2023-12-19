7293b1728f994ee7df5f210b066479b2b3190822
Ayman Ghorbel <ayman.ghorbel99@gmail.com>
2023-12-19 19:32:23 +0100

    add logoutService

--- src/main/java/com/example/springboot_mysql_template/security/SecuriteConfig.java
+++ src/main/java/com/example/springboot_mysql_template/security/SecuriteConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import static org.springframework.http.HttpMethod.*;
 import static org.springframework.http.HttpMethod.DELETE;
@@ -29,17 +30,8 @@ public class SecuriteConfig{
 
     private final AuthentificationFilter jwtFilter ;
     private final AuthenticationProvider authentificationProvider;
-    private static final String[] WHITE_LIST_URL = {"/api/v1/**",
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui/**",
-            "/webjars/**",
-            "/swagger-ui.html"};
+    private final LogoutHandler logoutHandler;
+    private static final String[] WHITE_LIST_URL = {"/api/v1/**"};
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http ) throws Exception {
 
@@ -61,13 +53,13 @@ public class SecuriteConfig{
                 .authenticationProvider(authentificationProvider)
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
-                /*.logout(logout ->
+                .logout(logout ->
                         logout.logoutUrl("/api/v1/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
                 )
 
-                 */
+
         ;
           return http.build();
 
